4196183cc8772aa9c8dfc939def5e7103a108d85
isybay <46872131+isybay@users.noreply.github.com>
2021-07-19 18:53:52 +0100

    Create Task_5.js

---
+++ Task_5.js
@@ -0,0 +1,16 @@
+{
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b9bd0868",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Task 5\n",
+    "\n",
+    "def replace_string():\n",
+    "    \n",
+    "    string = input(\"Enter string of characters\")\n",
+    "    \n",
+    "    return re.sub(' ', '%20', string)"
+   ]
+  }
